64e1cdfb09e15c577568866e9bcb3ff89b3d40e0
fedorovmichael <mishania.f@gmail.com>
2016-02-25 08:27:24 +0200

    added deploy's settings

---
+++ config/unicorn.rb
@@ -0,0 +1,9 @@
+root = '/opt/www/sampleapp/current'
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn_error.log"
+stdout_path "#{root}/log/unicorn.log"
+
+listen "/tmp/unicorn.sampleapp.sock"
+worker_processes 1
+timeout 30
